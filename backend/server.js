362ef603be92a708cb672c76704c75751906b3c8
luwamasmelash <liwix24@gmail.com>
2026-07-26 23:35:56 -0700

    server test

--- backend/server.js
+++ backend/server.js
@@ -3,33 +3,15 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
-import { clerkMiddleware, clerkClient, getAuth } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import userRouter from "./routes/userRoutes.js";
-import postRouter from "./routes/postRoutes.js";
-import storyRouter from "./routes/storyRoutes.js";
-import messageRouter from "./routes/messageRouter.js";
-
-
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-
 const app = express();
 
-
-app.use(express.json({
-  verify: (req, res, buf) => {
-    req.rawBody = buf;
-  }
-}));
 app.use(cors());
 
-
-await connectDB();
-
-// Inngest endpoint
 app.use(
   "/api/inngest",
   serve({
@@ -37,16 +19,17 @@ app.use(
     functions,
   })
 );
-app.use(clerkMiddleware())
-app.use('/api/user', userRouter)
-app.get('/', (req, res) => {
-  res.send('Server working')
-})
-app.use('/api/post', postRouter)
-app.use('/api/story', storyRouter)
-app.use('/api/message', messageRouter)
 
-console.log("Registered functions:", functions.map(fn => fn.id));
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+await connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server working");
+});
+
+app.listen(4000, () => {
+  console.log("Server running");
 });
